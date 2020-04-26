d243f4ea3eac27305f7eedddcedc522e86a6b6e4
jia <Jia Shi>
2020-04-26 17:32:56 -0400

    detect turnstile data

--- README.md
+++ README.md
@@ -24,3 +24,15 @@ python3 merge_files.py datasets/turnstile.txt
 ```
 python3 convert_data_from_txt_to_csv.py datasets/turnstile.txt datasets/turnstile.csv
 ```
+
+### detect turnstile
+
+```
+module load python/gnu/3.6.5
+module load spark/2.4.0 
+
+spark-submit --conf \
+spark.pyspark.python=/share/apps/python/3.6.5/bin/python \
+/home/js11182/COVID-19/turnstile_data_detect.py \
+/user/js11182/turnstile.csv
+```
---
+++ datasets/turnstile_data_detect.py
@@ -0,0 +1,21 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark import SparkContext
+
+# read data
+sc = SparkContext()
+spark = SparkSession.builder.appName("covid").config("spark.some.config.option", "some-value").getOrCreate()
+
+# Load Data
+turnstile = spark.read.format('csv').options(header = 'true', inferschema = 'true').load(sys.argv[1])
+
+
+# Detect
+
+# Distinct stations
+turnstile.createOrReplaceTempView("turnstile")
+distinct_station = spark.sql("SELECT STATION, COUNT(1) AS num FROM turnstile GROUP BY station ORDER BY STATION")
+
+distinct_station.coalesce(1).rdd.map(lambda x: ','.join(x)).saveAsTextFile("distinct-station.out")
+
