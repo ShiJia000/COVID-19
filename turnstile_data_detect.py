4352600c2497e9601bd2676a0c9f2cca980ce906
jia <Jia Shi>
2020-04-26 19:00:17 -0400

    add time detect

--- turnstile_data_detect.py
+++ turnstile_data_detect.py
@@ -20,8 +20,15 @@ distinct_station = spark.sql("SELECT STATION, COUNT(1) AS num FROM turnstile GRO
 distinct_station.coalesce(1).rdd.map(lambda x: x[0] + ',' + str(x[1])).saveAsTextFile("distinct-station.out")
 
 # Distinct Date & Time
-distinct_date = spark.sql("SELECT `date`, `time` FROM turnstile GROUP BY `date`, `time` ORDER BY `date`, `time`")
-distinct_date.coalesce(1).rdd.map(lambda x: x[0] + ',' + x[1]).saveAsTextFile("distinct-date.out")
+distinct_date = spark.sql("SELECT DISCINCT(`date`) FROM turnstile GROUP BY `date` ORDER BY `date`")
+distinct_date.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-date.out")
+
+# Distinct Time
+distinct_time= spark.sql("SELECT DISCINCT(`time`) FROM turnstile GROUP BY `time` ORDER BY `time`")
+distinct_time.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-time.out")
+
+# Find the date cannot be ordered, format the data to 'YYYY-MM-DD'
+
 
 
 # Data Clean
