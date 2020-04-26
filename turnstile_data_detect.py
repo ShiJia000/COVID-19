a782b18c20daae00436e867879ffcf414f001cd0
jia <Jia Shi>
2020-04-26 17:55:21 -0400

    fix bug detect

--- turnstile_data_detect.py
+++ turnstile_data_detect.py
@@ -15,8 +15,8 @@ turnstile.createOrReplaceTempView("turnstile")
 
 # Distinct stations
 distinct_station = spark.sql("SELECT STATION, COUNT(1) AS num FROM turnstile GROUP BY station ORDER BY STATION")
-distinct_station.coalesce(1).rdd.map(lambda x: ','.join(x)).saveAsTextFile("distinct-station.out")
+distinct_station.coalesce(1).rdd.map(lambda x: x[0] + ',' + str(x[1])).saveAsTextFile("distinct-station.out")
 
 # Distinct Date
 distinct_date = spark.sql("SELECT DISTINCT(`date`) FROM turnstile ORDER BY `date`")
-distinct_date.coalesce(1).rdd.map(lambda x: ','.join(x)).saveAsTextFile("distinct-date.out")
+distinct_date.coalesce(1).rdd.saveAsTextFile("distinct-date.out")
