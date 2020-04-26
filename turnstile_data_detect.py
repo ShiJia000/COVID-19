819e6e0d9343d6ce5f7c4201d521479c2ff47388
jia <Jia Shi>
2020-04-26 19:19:37 -0400

    add key collision

--- turnstile_data_detect.py
+++ turnstile_data_detect.py
@@ -15,6 +15,8 @@ turnstile.createOrReplaceTempView("turnstile")
 
 # Detect
 
+# Distinct
+
 # Distinct stations
 distinct_station = spark.sql("SELECT STATION, COUNT(1) AS num FROM turnstile GROUP BY station ORDER BY STATION")
 distinct_station.coalesce(1).rdd.map(lambda x: x[0] + ',' + str(x[1])).saveAsTextFile("distinct-station.out")
@@ -27,12 +29,27 @@ distinct_date.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-date.
 distinct_time = spark.sql("SELECT DISTINCT(`time`) FROM turnstile GROUP BY `time` ORDER BY `time`")
 distinct_time.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-time.out")
 
+# Distinct DESC
+distinct_desc = spark.sql("SELECT DISTINCT(`DESC`) FROM turnstile GROUP BY `DESC` ORDER BY `DESC`")
+distinct_desc.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-desc.out")
+
+# Distinct DIVISION
+distinct_division = spark.sql("SELECT DISTINCT(`DIVISION`) FROM turnstile GROUP BY `DIVISION` ORDER BY `DIVISION`")
+distinct_division.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-division.out")
+
+
+
 # key collision
-# distinct_key = spark.sql("SELECT DISTINCT(`time`) FROM turnstile GROUP BY `time` ORDER BY `time`")
-# distinct_time.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-time.out")
+distinct_key = spark.sql("SELECT`DATE`, `TIME`, `STATION`, `C/A`, `UNIT`, `SCP`, `DIVISION`, COUNT(1) AS collision_count \
+							FROM turnstile \
+							GROUP BY `C/A`, `UNIT`, `SCP`, `STATION`, `DIVISION`, `DATE`, `TIME` \
+							ORDER BY `collision_count`, `DATE`, `TIME`, `STATION`, `C/A`, `UNIT`, `SCP`, `DIVISION`")
+# `C/A`,`UNIT`, `SCP`, `STATION`, `LINENAME`, `DIVISION`, `DATE`, `TIME`
+distinct_key.coalesce(1).rdd.map(lambda x: x[0]).saveAsTextFile("distinct-key.out")
 
 # Find the date cannot be ordered, format the data to 'YYYY-MM-DD'
 
+# Find that they did not upload the data in same time.
 
 
 # Data Clean
