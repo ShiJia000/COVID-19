f1c32ffe98d135cb714e71be22c8b2f0a9f825f3
jia <Jia Shi>
2020-04-26 18:21:22 -0400

    fix bug

--- turnstile_data_detect.py
+++ turnstile_data_detect.py
@@ -3,6 +3,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark import SparkContext
 
+
+
 # read data
 sc = SparkContext()
 spark = SparkSession.builder.appName("covid").config("spark.some.config.option", "some-value").getOrCreate()
@@ -17,6 +19,11 @@ turnstile.createOrReplaceTempView("turnstile")
 distinct_station = spark.sql("SELECT STATION, COUNT(1) AS num FROM turnstile GROUP BY station ORDER BY STATION")
 distinct_station.coalesce(1).rdd.map(lambda x: x[0] + ',' + str(x[1])).saveAsTextFile("distinct-station.out")
 
-# Distinct Date
-distinct_date = spark.sql("SELECT DISTINCT(`date`) FROM turnstile ORDER BY `date`")
-distinct_date.coalesce(1).rdd.saveAsTextFile("distinct-date.out")
+# Distinct Date & Time
+distinct_date = spark.sql("SELECT `date`, `time` FROM turnstile GROUP BY `date`, `time` ORDER BY `date`, `time`")
+distinct_date.coalesce(1).rdd.map(lambda x: x[0] + ',' + x[1]).saveAsTextFile("distinct-date.out")
+
+
+# Data Clean
+
+# Remove the data of 2019
