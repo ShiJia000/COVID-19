fd81d8dd0af6cae76dc4cffa39a15ab3bc530d3f
fthongstar <haochz3@uci.edu>
2020-05-03 18:05:56 -0400

    update min& max

--- covid19_data_detect.py
+++ covid19_data_detect.py
@@ -10,6 +10,16 @@ spark = SparkSession.builder.appName("covid").config("spark.some.config.option",
 covid = spark.read.format('csv').options(header = 'true', inferschema = 'true').load(sys.argv[1])
 covid.createOrReplaceTempView("covid")
 
+#find min of positive cases
+min_positive = spark.sql("SELECT * FROM covid ORDER BY `POSITIVE` ASC LIMIT 10")
+min_positive.select(format_string("%s,%d,%d,%d,%.2f",date_format(min_positive["`DATE`"],'yyyy-MM-dd'),min_positive["`ZIPCODE`"],min_positive["`POSITIVE`"],min_positive["`TOTAL`"],min_positive["`RATE`"])) \
+       .write.save("min_positive.out",format="text")
+
+#find max of positive cases
+max_positive = spark.sql("SELECT * FROM covid ORDER BY `POSITIVE` DESC LIMIT 10")
+max_positive.select(format_string("%s,%d,%d,%d,%.2f",date_format(max_positive["`DATE`"],'yyyy-MM-dd'),max_positive["`ZIPCODE`"],max_positive["`POSITIVE`"],max_positive["`TOTAL`"],max_positive["`RATE`"])) \
+       .write.save("max_positive.out",format="text")
+
 #check illegal rate
 check_rate = spark.sql("SELECT * FROM covid WHERE `RATE` > 100")
 check_rate.coalesce(1).rdd.map(lambda x: ((x[0],x[1]),(x[2],x[3],[4]))).saveAsTextFile("illgeal_rate.out")
